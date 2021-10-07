1344d00a9bb5692ecf5491008a0e997ae46bad10
songtianlun <songtianlun@frytea.com>
2021-10-07 22:31:56 +0800

    first

---
+++ 19.删除链表的倒数第-n-个结点.1.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=golang
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getLength(head *ListNode) (length int) {
+	for ; head != nil; head = head.Next {
+		length++
+	}
+	return
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	length := getLength(head)
+	dummy := &ListNode{0, head}
+	cur := dummy
+	for i := 0; i < length-n; i++ {
+		cur = cur.Next
+	}
+	cur.Next = cur.Next.Next
+	return dummy.Next
+}
+
+// @lc code=end
+
