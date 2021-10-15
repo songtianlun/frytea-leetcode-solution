aeff12fd83e9def4a7c3b3f6892aad2947995e37
songtianlun <songtianlun@frytea.com>
2021-10-15 22:39:53 +0800

    [36] 有效的数独

---
+++ 36.有效的数独.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=36 lang=golang
+ *
+ * [36] 有效的数独
+ */
+
+// @lc code=start
+func isValidSudoku(board [][]byte) bool {
+	var rows, columns [9][9]int
+	var subboxws [3][3][9]int
+	for i, row := range board {
+		for j, c := range row {
+			if c == '.' {
+				continue
+			}
+			index := c - '1'
+			rows[i][index]++
+			columns[j][index]++
+			subboxws[i/3][j/3][index]++
+			if rows[i][index] > 1 ||
+				columns[j][index] > 1 ||
+				subboxws[i/3][j/3][index] > 1 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// @lc code=end
+
